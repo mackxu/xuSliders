e1d6f1bd689cbe9ed01e35a9bd4bcf38601af236
xu yadong <yadong.xu@jiepang.com>
2014-10-22 17:32:14 +0800

    完成结构搭建

---
+++ jquery.fullSlider.js
@@ -0,0 +1,33 @@
+;(function ( $, window, document, undefined ) {
+    // Create the defaults once
+    var pluginName = 'fullSlider',
+        defaults = {
+            
+        };
+
+    function Plugin( element, options ) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        // Place initialization logic here
+        
+    };
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    }
+
+})(jQuery, window);
